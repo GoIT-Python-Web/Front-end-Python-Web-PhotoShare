ce80870d9a78a845277286fe984790b2e1c7d016
xPhoenixUAx <pavloff12@gmail.com>
2025-03-30 19:03:27 +0300

    updatet button added arrow

--- src/components/UI/buttons/Button.jsx
+++ src/components/UI/buttons/Button.jsx
@@ -1,23 +1,52 @@
 import React from "react";
 import s from "./Button.module.css";
 
+const ArrowIcon = ({ position }) => (
+  <span className={`${s.arrow} ${s[position]}`}>
+    <svg
+      width="21"
+      height="14"
+      viewBox="0 0 21 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {position === "left" ? (
+        <path
+          d="M20.2129 6.96313H2.71289M8.40039 13.0881L2.27539 6.96313L8.40039 0.838135"
+          stroke="white"
+          strokeWidth="2"
+        />
+      ) : (
+        <path
+          d="M0.787109 7.03687H18.2871M12.5996 0.911866L18.7246 7.03687L12.5996 13.1619"
+          stroke="white"
+          strokeWidth="2"
+        />
+      )}
+    </svg>
+  </span>
+);
+
 const Button = ({
-  children,
-  onClick,
-  variant = "primary",
   size = "md",
+  variant = "primary",
   disabled = false,
-  className = "",
+  withArrow = false,
+  arrowPosition = "right",
+  children,
+  ...props
 }) => {
   return (
     <button
-      className={`${s.button} ${s[variant]} ${s[size]} ${
+      className={`${s.button} ${s[size]} ${s[variant]} ${
         disabled ? s.disabled : ""
-      } ${className}`}
-      onClick={onClick}
+      }`}
       disabled={disabled}
+      {...props}
     >
+      {withArrow && arrowPosition === "left" && <ArrowIcon position="left" />}
       {children}
+      {withArrow && arrowPosition === "right" && <ArrowIcon position="right" />}
     </button>
   );
 };
