666483f58903d0c62adfecacc984d3c2f8b5ce08
Oleksii Ozyrskyi <aozyrskyii@gmail.com>
2025-03-31 23:13:13 +0200

    started filter popup

--- src/components/posts/postItem/PostItem.jsx
+++ src/components/posts/postItem/PostItem.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import { IoIosResize } from "react-icons/io";
 import Stars from "../../../helpers/Stars.jsx";
 import css from "./PostItem.module.css";
 import Button from "../../UI/buttons/Button.jsx";
@@ -6,15 +10,28 @@ import { useMediaQuery } from "react-responsive";
 export default function PostItem({ post }) {
   const isDesktop = useMediaQuery({ minWidth: 1440 });
   const isTablet = useMediaQuery({ minWidth: 768 });
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <li className={css.li}>
-      <img
-        src={post.post_pic}
-        alt="Post's picture"
-        width={328}
-        height={328}
-        className={css.postPic}
-      />
+      <div className={css.imageWrapper}>
+        <img src={post.post_pic} alt="Post's picture" className={css.postPic} />
+
+        <button className={css.viewButton} onClick={() => setIsOpen(true)}>
+          <IoIosResize className={css.resize} />
+        </button>
+      </div>
+
+      {isOpen && (
+        <Lightbox
+          open={isOpen}
+          close={() => setIsOpen(false)}
+          slides={[{ src: post.post_pic }]}
+          carousel={{ finite: true, preload: 1 }}
+        />
+      )}
+
       <div className={css.credentials}>
         <img
           src={post.photo}
@@ -24,13 +41,14 @@ export default function PostItem({ post }) {
         />
         <p>{post.user_name}</p>
       </div>
+
       <div className={css.postCredentials}>
         <p className={css.title}>{post.title}</p>
         <p className={css.tags}>{post.tags.join(" ")}</p>
         <div className={css.ratingDiv}>
           <Stars rating={post.rating} />
           <p className={css.rating}>
-            {post.rating}({post.rating_length} оцінок)
+            {post.rating} ({post.rating_length} оцінок)
           </p>
         </div>
         <div className={css.dateDiv}>
@@ -38,6 +56,7 @@ export default function PostItem({ post }) {
           <p className={css.createdAt}>{post.createdAt}</p>
         </div>
       </div>
+
       <Button size={isDesktop ? "xxl" : isTablet ? "xl" : "lg"}>
         Детальніше
       </Button>
--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,12 +1,18 @@
-import React from "react";
 import Title from "../../components/UI/title/Title";
 import PostsList from "../../components/posts/postsList/PostsList.jsx";
+import css from "./MainPage.module.css";
+import Filters from "../../components/filters/filters/Filters.jsx";
+import FilterPopUp from "../../components/filters/filterPopUp/FilterPopUp.jsx";
 
 export default function MainPage() {
   return (
     <div className="container">
-      <Title location="main" />
-      <PostsList />
+      <div className={css.headWrapper}>
+        <Title location="main" />
+        <Filters />
+      </div>
+      {/* <PostsList /> */}
+      <FilterPopUp />
     </div>
   );
 }
