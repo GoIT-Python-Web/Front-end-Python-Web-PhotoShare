16fd4e0f576f2cd73689196ce5c22152e7278f17
Oleksii Ozyrskyi <aozyrskyii@gmail.com>
2025-04-02 13:12:46 +0200

    clear app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import Header from "./components/Header/Header.jsx";
 import "./index.css";
-import MainPage from "./pages/main/MainPage.jsx";
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <MainPage />
-    </>
-  );
+  return <></>;
 }
