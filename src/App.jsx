3b8ebd4922d7b23a4de01eb40c112f4736db62c3
SSovelich <ssovelich@gmail.com>
2025-04-03 15:49:53 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Layout from "./Layout";
 import { lazy } from "react";
+import RestrictedRoute from "./components/RestrictedRoute";
 
 export default function App() {
   const MainPage = lazy(() => import("./pages/main/MainPage.jsx"));
   const ProfilePage = lazy(() => import("./pages/profile/ProfilePage.jsx"));
+  const SignUpPage = lazy(() => import("./pages/auth/SignUpPage.jsx"));
+
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Navigate to="/posts" replace />} />
         <Route path="/posts" element={<MainPage />} />
         <Route path="/my-profile" element={<ProfilePage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute redirectTo="/my-profile" component={<SignUpPage />} />
+          }
+        />
       </Routes>
     </Layout>
   );
