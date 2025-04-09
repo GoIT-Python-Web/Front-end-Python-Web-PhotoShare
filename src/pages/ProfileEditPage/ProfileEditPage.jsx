7b2d9c78fd369580a03571dd71f0453b923b5ff1
SSovelich <ssovelich@gmail.com>
2025-04-09 22:25:29 +0300

    commit

--- src/pages/ProfileEditPage/ProfileEditPage.jsx
+++ src/pages/ProfileEditPage/ProfileEditPage.jsx
@@ -1,15 +1,11 @@
-// import css from "../ProfileEditPage/ProfileEditPage.module.css";
-
 import ProfileEditForm from "../../components/forms/ProfileEditForm/ProfileEditForm.jsx";
-
 
 const ProfileEditPage = () => {
   
 
   return (
     <div className="container">
-      
-      <ProfileEditForm />
+        <ProfileEditForm />
     </div>
   );
 };
